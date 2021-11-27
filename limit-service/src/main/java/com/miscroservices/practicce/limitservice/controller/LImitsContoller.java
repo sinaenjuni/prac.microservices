dc7acb168284eb11d5af32f2c96bc6c4d4da3fc1
sinaenjuni <y2k4388@gmail.com>
2021-11-27 22:58:43 +0900

    add spring-cloud-config-server

--- limit-service/src/main/java/com/miscroservices/practicce/limitservice/controller/LImitsContoller.java
+++ limit-service/src/main/java/com/miscroservices/practicce/limitservice/controller/LImitsContoller.java
@@ -1,15 +1,22 @@
 package com.miscroservices.practicce.limitservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.miscroservices.practicce.limitservice.bean.Limits;
+import com.miscroservices.practicce.limitservice.configuration.Configuration;
 
 @RestController
 public class LImitsContoller {
 	
+	@Autowired
+	private Configuration configuration;
+	
 	@GetMapping("/limits")
 	public Limits retrieveLimits() {
-		return new Limits(1, 1000);
+		
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
+//		return new Limits(1, 1000);
 	}
 }
